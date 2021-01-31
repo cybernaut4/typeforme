6d90747a6d44acc0ae4ec4bff3bedbb176710c81
Cybernaut <cybernaut4@protonmail.com>
2021-01-31 15:01:02 -0300

    Added --help, +blink options. Try & catch implementation.
    typeSpeed default to 60 (from 100)
    The if condition is now inside the for conditional.
    The i variable renamed to pos (as "position")
    --speed and --delay now have try-catchers (IndexOutOfRange and FormatException)
    +blink and -blink added.
    -color-fg and bg variants renamed to --fg-reset and --bg-reset
    Added --help documentation.

--- Program.cs
+++ Program.cs
@@ -8,24 +8,47 @@ namespace typeforme
 
 		static void Main(string[] args)
 		{
-		  int typeSpeed = 100;
+		  int typeSpeed = 60;
 			bool isSwitchSetup = false;
 
-			for (int i=0; i < args.Length; i++) 
+			if (args.Length == 0){
+					WriteUsage();
+					WriteExamples();
+					Console.WriteLine("For more info, try the --help option.");
+			}
+			for (
+							int pos = 0; 
+							pos < args.Length; 
+							pos = isSwitchSetup ? pos+2 : pos+1, isSwitchSetup = false
+				  ) 
 			{
-					if (isSwitchSetup) {
-							isSwitchSetup = false;
-							continue;
-					}
-
-					switch (args[i]) {
+					switch (args[pos]) {
+						case "--help":
+								WriteHelp();
+								break;
 						case "--speed":	
-								typeSpeed = Convert.ToInt32(args[i+1]); 
+								try {
+										typeSpeed = Convert.ToInt32(args[pos+1]); 
+								}
+								catch(System.IndexOutOfRangeException) {
+										break;
+								}
+								catch(System.FormatException) {
+										Console.Write(args[pos+1]);
+								}
 								isSwitchSetup = true; 
 								continue;
 
 						case "--delay":
-								Thread.Sleep(Convert.ToInt32(args[i+1]));
+								try {
+										Thread.Sleep(Convert.ToInt32(args[pos+1]));
+								}
+								catch (System.IndexOutOfRangeException) {
+										break;
+								}
+								catch (System.FormatException) {
+										Console.Write(args[pos+1]);
+								}
 								isSwitchSetup = true;
 								continue;
 
@@ -39,6 +62,7 @@ namespace typeforme
 						case "+bold": 					Console.Write(TextFormat.bold); continue;
 						case "+italic": 				Console.Write(TextFormat.italic); continue;
 						case "+underline": 			Console.Write(TextFormat.underline); continue;
+						case "+blink": 					Console.Write(TextFormat.blink); continue;
 						case "+highlight":			Console.Write(TextFormat.highlight); continue;
 						case "+strikethrough": 	Console.Write(TextFormat.strikethrough); continue;
 						case "+underline-dual":	Console.Write(TextFormat.underlineDual); continue;
@@ -46,11 +70,12 @@ namespace typeforme
 						case "-bold": 					Console.Write(TextFormat.noBold); continue;
 						case "-italic": 				Console.Write(TextFormat.noItalic); continue;
 						case "-underline": 			Console.Write(TextFormat.noUnderline); continue;
+						case "-blink": 					Console.Write(TextFormat.noBlink); continue;
 						case "-highlight":			Console.Write(TextFormat.noHighlight); continue;
 						case "-strikethrough": 	Console.Write(TextFormat.noStrikethrough); continue;
 
-						case "-color-fg":	Console.Write(TextFormat.noColor); continue;
-						case "-color-bg":	Console.Write(BackgroundFormat.noColor); continue;
+						case "--fg-reset":		Console.Write(TextFormat.noColor); continue;
+						case "--bg-reset":		Console.Write(BackgroundFormat.noColor); continue;
 
 						case "+color-fg-dark-gray":			Console.Write(TextFormat.darkGray); continue;
 						case "+color-fg-dark-red":			Console.Write(TextFormat.darkRed); continue;
@@ -89,9 +114,9 @@ namespace typeforme
 						case "+color-bg-bright-white":	Console.Write(BackgroundFormat.brightWhite); continue;
 
 						default: {
-								for (int j=0; j < args[i].Length; j++)
+								for (int i=0; i < args[pos].Length; i++)
 								{
-										Console.Write(args[i][j]); 
+										Console.Write(args[pos][i]); 
 										Thread.Sleep(typeSpeed);
 								}
 								continue;
@@ -99,6 +124,67 @@ namespace typeforme
 				  }
 				}
 			Console.Write($"{Format.reset}\n");
+		}
+
+		static void WriteUsage() {
+				Console.WriteLine("");
+				Console.WriteLine("Usage:");
+				Console.WriteLine("\ttypeforme ['text']...");
+				Console.WriteLine("\ttypeforme [options] ['text']...");
+				Console.WriteLine("");
+		}
+
+		static void WriteExamples() {
+				Console.WriteLine("Examples:");
+				Console.WriteLine("\ttypeforme 'I am the alpha and the omega'");
+				Console.WriteLine("\ttypeforme +bold +color-fg-dark-green 'I am the alpha and the omega'");
+				Console.WriteLine("\ttypeforme +bold +color-fg-dark-green 'I am the ' \\");
+				Console.WriteLine("\t\t  +color-fg-bright-green 'alpha ' \\");
+				Console.WriteLine("\t\t  +color-fg-dark-green 'and the ' \\");
+				Console.WriteLine("\t\t  +color-fg-bright-green 'omega'");
+				Console.WriteLine("");
+		}
+ 
+		static void WriteHelp() {
+			Console.WriteLine("");
+			Console.WriteLine("typeforme - Show a formatteable message, character by character.");	
+			WriteUsage();
+			Console.WriteLine("Options:");
+			Console.WriteLine("\t--help\t\t\tDisplay this usage information.");
+			Console.WriteLine("\t--speed <integer>\tChanges the typing speed by milliseconds.");
+			Console.WriteLine("\t--delay <integer>\tPuts this program to sleep for milliseconds");
+			Console.WriteLine("\t\t\t\tbefore resuming.");
+			Console.WriteLine("Formatting options:");
+			Console.WriteLine("\t--reset\t\tDisables all formatting options onwards.");
+			Console.WriteLine("\t--break\t\tBreaks line.");
+			Console.WriteLine("\t--newline\tSame as --break, but also applies --reset beforehand.");
+			Console.WriteLine("");
+			Console.WriteLine("\tEnabling\t\tDisabling");
+			Console.WriteLine("\t--------\t\t---------");
+			Console.WriteLine("\t+bold · · · · · · · ·  \t-bold");
+			Console.WriteLine("\t+italic · · · · · · ·  \t-italic");
+			Console.WriteLine("\t+underline  · · · · ·  \t-underline");
+			Console.WriteLine("\t+blink  · · · · · · ·  \t-blink");
+			Console.WriteLine("\t+highlight  · · · · ·  \t-highlight");
+			Console.WriteLine("\t+strikethrough  · · ·  \t-strikethrough");
+			Console.WriteLine("\t+underline-dual · · ·  \t-underline");
+			Console.WriteLine("");
+			Console.WriteLine("Coloring Options:");
+  		Console.WriteLine("\t+color-[fg|bg]-[dark|bright]-gray\t");
+		  Console.WriteLine("\t+color-[fg|bg]-[dark|bright]-red\t");
+		  Console.WriteLine("\t+color-[fg|bg]-[dark|bright]-green\t");
+		  Console.WriteLine("\t+color-[fg|bg]-[dark|bright]-yellow\t");
+		  Console.WriteLine("\t+color-[fg|bg]-[dark|bright]-blue\t");
+		  Console.WriteLine("\t+color-[fg|bg]-[dark|bright]-purple\t");
+		  Console.WriteLine("\t+color-[fg|bg]-[dark|bright]-cyan\t");
+		  Console.WriteLine("\t+color-[fg|bg]-[dark|bright]-white\t");
+			Console.WriteLine("");
+			Console.WriteLine("\tfg and bg are foreground (text) and background, respectively.");
+			Console.WriteLine("");
+			Console.WriteLine("\t--fg-reset\tRestores foreground color to default.");
+			Console.WriteLine("\t--bg-reset\tRestores background color to default.");
+			Console.WriteLine("");
+			WriteExamples();
 		}
   }
 }
